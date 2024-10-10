ff5ca3732378c5e0be93df855a0e4b3f6dc3300c
jml-mag <michael@matterandgas.com>
2024-10-10 12:18:53 -0500

    added custom domain for verification

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -8,7 +8,8 @@
  * This file defines the backend configuration for the application using AWS Amplify.
  * It imports and integrates the authentication and data resources into the backend setup.
  */
-
+import { Stack } from "aws-cdk-lib/core"
+import { EmailIdentity } from "aws-cdk-lib/aws-ses"
 import { defineBackend } from '@aws-amplify/backend'; // Import the defineBackend function from AWS Amplify.
 import { auth } from './auth/resource'; // Import the authentication resource configuration.
 import { data } from './data/resource'; // Import the data resource configuration.
@@ -18,9 +19,38 @@ import { manageUsers } from './functions/manage-users/resource'; // Import the m
  * Backend Configuration
  * ---------------------
  * This function call sets up the backend by integrating the auth, data, and manageUsers configurations.
- */
+ 
 defineBackend({
   auth,
   data,
   manageUsers,
 });
+*/
+
+/**
+ * Backend Configuration
+ * ---------------------
+ * This sets up the backend by integrating the auth, data, and manageUsers configurations as well as AWS SES for production email.
+ */
+
+const backend = defineBackend({
+  auth,
+  data,
+  manageUsers,
+})
+
+const { cfnUserPool } = backend.auth.resources.cfnResources
+const authStack = Stack.of(cfnUserPool)
+
+const email = EmailIdentity.fromEmailIdentityName(
+  authStack,
+  "EmailIdentity",
+  // your email configured for use in SES
+  //process.env.EMAIL || ''
+  "no-reply@meetadrifter.com"
+)
+
+cfnUserPool.emailConfiguration = {
+  emailSendingAccount: "DEVELOPER",
+  sourceArn: email.emailIdentityArn,
+}
